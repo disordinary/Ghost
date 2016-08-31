b893ed2b57a30116350ad37946275a2e079fb885
Ryan McCarvill <ryan@killa.linux>
2016-09-01 00:02:55 +1200

    replace URL of internal images test

--- core/server/apps/image-optimisation/index.js
+++ core/server/apps/image-optimisation/index.js
@@ -2,8 +2,18 @@ var config     = require('../../config'),
     errors     = require('../../errors'),
     i18n       = require('../../i18n');
 
+
+
+
+
 module.exports = {
-    activate: function activate() {
-        console.log("ACTIVATED BEATCH!");
+    activate: function activate(app) {
+        app.filters.register("prePostsRender" , function( post) {
+
+            post.html = post.html.replace(/(<img.*src=["|'])(\/content\/images\/)([^"&^']*["|'])/g,
+                "$1http://ghost.org/$3");
+
+            return post;
+        });
     }
 };
