3cf1820e7c4ec1c07884bee30cb2606177f25acd
Ryan McCarvill <ryan@disordinary.com>
2017-04-12 20:08:23 +1200

    🐝  Allow inbalanced HTML in markdown card.
    The simpledom interpreter that the Mobiledoc DOM renderer uses does not allow for inbalanced or incorrect HTML such as that which is entered by a user.
    
    This PR adds a step where the HTML is sanitised and balanced before being passed to simpledom.

--- core/server/apps/default-cards/cards/markdown.js
+++ core/server/apps/default-cards/cards/markdown.js
@@ -1,14 +1,17 @@
 var SimpleDom   = require('simple-dom'),
     tokenizer   = require('simple-html-tokenizer').tokenize,
+    jsdom       = require('jsdom').jsdom,
     Showdown    = require('showdown-ghost'),
     converter   = new Showdown.converter({extensions: ['ghostgfm', 'footnotes', 'highlight']}),
-    parser;
+    parser,
+    sanitisedHTML;
 
 module.exports = {
         name: 'card-markdown',
         type: 'dom',
         render(opts) {
+            sanitisedHTML = jsdom(converter.makeHtml(opts.payload.markdown || '')).body.innerHTML;
             parser = new SimpleDom.HTMLParser(tokenizer, opts.env.dom, SimpleDom.voidMap);
-            return parser.parse('<div class="kg-card-markdown">' + converter.makeHtml(opts.payload.markdown || '') + '</div>');
+            return parser.parse('<div class="kg-card-markdown">' + sanitisedHTML  + '</div>');
         }
     };
